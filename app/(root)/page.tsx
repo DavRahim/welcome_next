ebac992021d332aaf05ed44f91f75952729c3f72
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2025-01-14 21:52:15 +0600

    project work 50% done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
+import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
     searchParams,
@@ -6,8 +10,27 @@ export default async function Home({
     searchParams: Promise<{ query?: string }>;
 }) {
     const query = (await searchParams).query;
-    // const params = { search: query || null };
-    // const session = await auth();
+    const params = { search: query || null };
+
+    const session = await auth();
+
+    console.log(session?.user?.id);
+    const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
+    // const posts = [{
+    //     _createdAt: new Date(),
+    //     views: 55,
+    //     author: {
+    //         _id: 1,
+    //         name: "Rahim",
+    //         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnoniL4yagtAaB0FkoOZM-EGN72M8DEKFuMA&s",
+    //     },
+    //     title: "We Robots",
+    //     category: 'Robots',
+    //     _id: 1,
+    //     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnoniL4yagtAaB0FkoOZM-EGN72M8DEKFuMA&s",
+    //     description: "This is a Description",
+    // }]
     return (
         <>
             <section className="pink_container">
@@ -22,7 +45,22 @@ export default async function Home({
                 </p>
                 <SearchForm query={query} />
             </section>
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `search result for "${query}"` : "All Startups"}
+                </p>
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupTypeCard) => (
+                            <StartupCard key={post?._id} post={post} />
+                        ))
+                    ) : (
+                        <p className="no-results">No startups found</p>
+                    )}
+                </ul>
+            </section>
 
+            <SanityLive />
         </>
     )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        
+      },
+    ],
+  },
+  experimental: {
+    ppr: "incremental",
+    after: true,
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
   /* config options here */
 };
 
